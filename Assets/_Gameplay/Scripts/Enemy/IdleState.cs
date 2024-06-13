fec0765a044b4d0a682b9abbb2d498f5972dfacd
ducmanh <ducmanh.kt23@gmail.com>
2024-06-13 11:12:41 +0700

    ~ Fix lại luồng change state của enemy

--- Assets/_Gameplay/Scripts/Enemy/IdleState.cs
+++ Assets/_Gameplay/Scripts/Enemy/IdleState.cs
@@ -8,25 +8,23 @@ public class IdleState : IState<Enemy>
     public void OnEnter(Enemy t)
     {
         t.ChangeAnim(Constain.ANIM_IDLE);
+        t.NearestEnemy();
     }
 
     public void OnExecute(Enemy t)
     {
         timer += Time.deltaTime;
-        
-        if(timer > 1f)
+        if (t.nearestCharacter != null)
         {
-            t.NearestEnemy();
-            if (t.nearestCharacter != null)
-            {
-                t.ChangeState(new AttackState());
-            }
-            else
+            t.ChangeState(new AttackState());
+        }
+        else
+        {
+            if(timer > 1f)
             {
                 t.ChangeState(new MoveState());
             }
-
-        }    
+        }  
     }
 
     public void OnExit(Enemy t)
